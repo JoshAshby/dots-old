a3e11cc4bee4bcd7d93b3155578b382de4fe82eb
JoshAshby <joshuaashby@joshashby.com>
2013-05-28 15:45:48 -0600

    Minor typo

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -15,7 +15,7 @@ alias sigyn='ssh -X joshashby.com'
 #Git stuff
 alias gs="git status"
 alias gdiff="git diff"
-alias gl="git pull"
+alias gpl="git pull"
 alias gf="git fetch"
 alias gm="git merge"
 alias gps="git push"
