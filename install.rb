1483ed5c3790ea5e201ef86daa726b9d0b9d3cae
JoshAshby <joshuaashby@joshashby.com>
2015-02-06 16:41:43 -0700

    install changes

--- install.rb
+++ install.rb
@@ -1,7 +1,15 @@
 #!/usr/bin/env ruby
 require 'fileutils'
+require 'optparse'
 
-overwrite_files = true
+options = {}
+OptionParser.new do |opts|
+  opts.banner = "Usage: install.rb [options]"
+
+  opts.on '-f', '--force', 'Force overwriting files' do |v|
+    options[:force] = v
+  end
+end.parse!
 
 home = Dir.home
 repo = Dir.pwd
@@ -12,7 +20,13 @@ dont_link = {
   linux: ['.slate.js']
 }
 
-except = dont_link[:all] + dont_link['linux']
+if RUBY_PLATFORM =~ /darwin/
+  platform = :osx
+else
+  platform = :linux
+end
+
+except = dont_link[:all] + dont_link[platform]
 
 # Well this is messy...
 to_link = (Dir.entries('.') - except).map do |dir|
@@ -29,7 +43,7 @@ to_link.flatten!
 
 unless Dir.exist? "#{home}/.oh-my-zsh"
   puts "Installing oh-my-zsh..."
-  #`curl -L http://install.ohmyz.sh | sh`
+  `curl -L http://install.ohmyz.sh | sh`
 end
 
 
@@ -38,17 +52,15 @@ to_link.each do |file|
   home_file = File.join home, file
   repo_file = File.join repo, file
 
-  exists = File.exist?(home_file) || File.symlink?(home_file)
-
-  if exists
-    if overwrite_files
-      puts "\t\e[0;33mFile #{home_file} exists, but still linking #{repo_file} --> #{home_file}"
-      FileUtils.symlink repo_file, home_file, force: true
+  begin
+    puts "\t\e[0;32mTrying to link #{repo_file} --> #{home_file}"
+    FileUtils.symlink repo_file, home_file
+  rescue Errno::EEXIST => e
+    if options[:force]
+      puts "\t\e[0;33mFile #{home_file} exists, linking anyways #{repo_file} --> #{home_file}"
+      FileUtils.symlink repo_file, home_file, force: options[:force]
     else
       puts "\t\e[0;31mFile #{home_file} exists already, remove and rerun to link it"
     end
-  else
-    puts "\t\e[0;32mLinking #{repo_file} --> #{home_file}"
-    FileUtils.symlink repo_file, home_file
   end
 end
