6e67144e49b000564ce17f0f64bfc7b503b7b539
JoshAshby <joshuaashby@joshashby.com>
2016-06-17 11:40:45 -0600

    update hammerspoon to handle screens

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -32,7 +32,7 @@ end
 hs.screen.watcher.new(onScreenChange):start()
 
 hs.window.animationDuration = 0
-hs.window.setFrameCorrectness = true
+--hs.window.setFrameCorrectness = true
 
 -- modelyolo, mutation and state were stolen from robacarps hammerspoon here\
 -- https://github.com/robacarp/config_files/blob/master/.hammerspoon/init.lua
@@ -42,7 +42,7 @@ hs.window.setFrameCorrectness = true
 ModalYoLo = {}
 function ModalYoLo:new(modal_key)
   local obby = {
-    binding = hs.hotkey.modal.new('',modal_key)
+    binding = hs.hotkey.modal.new('', modal_key)
   }
 
   -- disable binding on escape key
@@ -52,7 +52,7 @@ function ModalYoLo:new(modal_key)
 
   -- automatically disable modal key after 1 second
   function obby.binding:entered()
-    hs.timer.doAfter(1, function()
+    hs.timer.doAfter(2, function()
       obby.binding:exit()
     end)
   end
@@ -77,8 +77,9 @@ function Mutation:new()
     frame_y = 0,
     frame_w = 1000,
     frame_h = 1000,
-    screen  = hs.screen.mainScreen():fullFrame(),
-    window  = hs.window.focusedWindow()
+    window  = hs.window.focusedWindow(),
+    screen  = hs.window.focusedWindow():screen(),
+    frame   = hs.window.focusedWindow():screen():fullFrame()
   }
 
   self.__index = self
@@ -86,15 +87,12 @@ function Mutation:new()
 end
 
   function Mutation:debug()
-   -- hs.alert.show(
-    --   "Heights: frame:" .. scr:frame().h .. ", fullFrame:" .. scr:fullFrame().h .. "\n" ..
-    --   "\tframe / fullFrame:" .. scr:frame().h / scr:fullFrame().h
-    -- )
-
     hs.alert.show(
       "X / Y: " .. self.frame_x .. ", " .. self.frame_y .. "\n" ..
-      "W / H: " .. self.frame_w .. ", " .. self.frame.h
+      "W / H: " .. self.frame_w .. ", " .. self.frame_h
     , 3)
+
+    self:commit()
   end
 
   function Mutation:x(ex)
@@ -118,12 +116,12 @@ end
   end
 
   function Mutation:adjust_frame()
-    screen = hs.screen.mainScreen()
-    local y_offset = screen:fullFrame().h - screen:frame().h
-    local x_offset = screen:fullFrame().w - screen:frame().w
+    local_frame = hs.window.focusedWindow():screen():frame()
+    local y_offset = self.frame.h - local_frame.h
+    local x_offset = self.frame.w - local_frame.w
 
-    h_multiplier = screen:frame().h / screen:fullFrame().h
-    w_multiplier = screen:frame().w / screen:fullFrame().w
+    h_multiplier = local_frame.h / self.frame.h
+    w_multiplier = local_frame.w / self.frame.w
 
     self.frame_x = self.frame_x * w_multiplier + x_offset
     self.frame_y = self.frame_y * h_multiplier + y_offset
@@ -133,7 +131,8 @@ end
 
   function Mutation:commit()
     self:adjust_frame()
-    self.window:setFrame(self:buildFrame())
+    frame = self.screen:localToAbsolute(self:buildFrame())
+    self.window:setFrame(frame)
   end
 
   function Mutation:buildFrame()
@@ -144,6 +143,16 @@ end
       self.frame_h
     )
   end
+
+-- Debugging mouse positions
+--icon = hs.menubar.new()
+--icon:setTitle("MP: ")
+
+--hs.timer.doEvery(0.5, function()
+--  mouse = hs.mouse.getAbsolutePosition()
+
+--  icon:setTitle("MP: " .. mouse.x .. " " .. mouse.y .. "")
+--end)
 
 -- Keys for screen resize are generally laid out as the following:
 -- [ e r t ]
@@ -164,47 +173,47 @@ half_modal = ModalYoLo:new('f6')
 
 -- Top Left corner
 half_modal:bind('e', function(mutator)
-  mutator:x(0):y(0):w(mutator.screen.w / 2):h(mutator.screen.h / 2):commit()
+  mutator:x(0):y(0):w(mutator.frame.w / 2):h(mutator.frame.h / 2):commit()
 end)
 
 -- Top half
 half_modal:bind('r', function(mutator)
-  mutator:x(0):y(0):w(mutator.screen.w):h(mutator.screen.h / 2):commit()
+  mutator:x(0):y(0):w(mutator.frame.w):h(mutator.frame.h / 2):commit()
 end)
 
 -- Top Right corner
 half_modal:bind('t', function(mutator)
-  mutator:x(mutator.screen.w / 2):y(0):w(mutator.screen.w / 2):h(mutator.screen.h / 2):commit()
+  mutator:x(mutator.frame.w / 2):y(0):w(mutator.frame.w / 2):h(mutator.frame.h / 2):commit()
 end)
 
 -- Left half
 half_modal:bind('d', function(mutator)
-  mutator:x(0):y(0):w(mutator.screen.w / 2):h(mutator.screen.h):commit()
+  mutator:x(0):y(0):w(mutator.frame.w / 2):h(mutator.frame.h):commit()
 end)
 
 -- Full Screen
 half_modal:bind('f', function( mutator)
-  mutator:x(0):y(0):w(mutator.screen.w):h(mutator.screen.h):commit()
+  mutator:x(0):y(0):w(mutator.frame.w):h(mutator.frame.h):commit()
 end)
 
 -- Right half
 half_modal:bind('g', function(mutator)
-  mutator:x(mutator.screen.w / 2):y(0):w(mutator.screen.w / 2):h(mutator.screen.h):commit()
+  mutator:x(mutator.frame.w / 2):y(0):w(mutator.frame.w / 2):h(mutator.frame.h):commit()
 end)
 
 -- Bottom Left corner
 half_modal:bind('c', function(mutator)
-  mutator:x(0):y(mutator.screen.h / 2):w(mutator.screen.w / 2):h(mutator.screen.h / 2):commit()
+  mutator:x(0):y(mutator.frame.h / 2):w(mutator.frame.w / 2):h(mutator.frame.h / 2):commit()
 end)
 
 -- Bottom half
 half_modal:bind('v', function(mutator)
-  mutator:x(0):y(mutator.screen.h / 2):w(mutator.screen.w):h(mutator.screen.h / 2):commit()
+  mutator:x(0):y(mutator.frame.h / 2):w(mutator.frame.w):h(mutator.frame.h / 2):commit()
 end)
 
 -- Bottom Right corner
 half_modal:bind('b', function(mutator)
-  mutator:x(mutator.screen.w / 2):y(mutator.screen.h / 2):w(mutator.screen.w / 4):h(mutator.screen.h / 4):commit()
+  mutator:x(mutator.frame.w / 2):y(mutator.frame.h / 2):w(mutator.frame.w / 2):h(mutator.frame.h / 2):commit()
 end)
 
 -- 80% size modal
@@ -212,47 +221,47 @@ eighty_percent_modal = ModalYoLo:new('f5')
 
 -- Top Left corner
 eighty_percent_modal:bind('e', function(mutator)
-  mutator:x(0):y(0):w(mutator.screen.w - 150):h(mutator.screen.h - 150):commit()
+  mutator:x(0):y(0):w(mutator.frame.w - 150):h(mutator.frame.h - 150):commit()
 end)
 
 -- Top eighty_percent
 eighty_percent_modal:bind('r', function(mutator)
-  mutator:x(0):y(0):w(mutator.screen.w):h(mutator.screen.h - 150):commit()
+  mutator:x(0):y(0):w(mutator.frame.w):h(mutator.frame.h - 150):commit()
 end)
 
 -- Top Right corner
 eighty_percent_modal:bind('t', function(mutator)
-  mutator:x(150):y(0):w(mutator.screen.w - 150):h(mutator.screen.h - 150):commit()
+  mutator:x(150):y(0):w(mutator.frame.w - 150):h(mutator.frame.h - 150):commit()
 end)
 
 -- Left eighty_percent
 eighty_percent_modal:bind('d', function(mutator)
-  mutator:x(0):y(0):w(mutator.screen.w - 150):h(mutator.screen.h):commit()
+  mutator:x(0):y(0):w(mutator.frame.w - 150):h(mutator.frame.h):commit()
 end)
 
 -- Full Screen
 eighty_percent_modal:bind('f', function( mutator)
-  mutator:x(150):y(150):w(mutator.screen.w - 300):h(mutator.screen.h - 300):commit()
+  mutator:x(150):y(150):w(mutator.frame.w - 300):h(mutator.frame.h - 300):commit()
 end)
 
 -- Right eighty_percent
 eighty_percent_modal:bind('g', function(mutator)
-  mutator:x(150):y(0):w(mutator.screen.w - 150):h(mutator.screen.h):commit()
+  mutator:x(150):y(0):w(mutator.frame.w - 150):h(mutator.frame.h):commit()
 end)
 
 -- Bottom Left corner
 eighty_percent_modal:bind('c', function(mutator)
-  mutator:x(0):y(150):w(mutator.screen.w - 150):h(mutator.screen.h - 150):commit()
+  mutator:x(0):y(150):w(mutator.frame.w - 150):h(mutator.frame.h - 150):commit()
 end)
 
 -- Bottom eighty_percent
 eighty_percent_modal:bind('v', function(mutator)
-  mutator:x(0):y(150):w(mutator.screen.w):h(mutator.screen.h - 150):commit()
+  mutator:x(0):y(150):w(mutator.frame.w):h(mutator.frame.h - 150):commit()
 end)
 
 -- Bottom Right corner
 eighty_percent_modal:bind('b', function(mutator)
-  mutator:x(150):y(150):w(mutator.screen.w - 150):h(mutator.screen.h - 150):commit()
+  mutator:x(150):y(150):w(mutator.frame.w - 150):h(mutator.frame.h - 150):commit()
 end)
 
 
