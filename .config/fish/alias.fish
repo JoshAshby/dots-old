6b643117bb6811c4ffb1dc9299f5f2a5737482bb
JoshAshby <joshuaashby@joshashby.com>
2018-12-18 09:28:59 -0700

    docker compose aliases

--- .config/fish/alias.fish
+++ .config/fish/alias.fish
@@ -11,3 +11,10 @@ alias mk 'mkdir'
 alias nope "echo 'Oh, okay... :('"
 
 alias c '/bin/cat'
+
+alias dcb 'docker-compose build'
+alias dcu 'docker-compose up'
+alias dcs 'docker-compose start'
+alias dcr 'docker-compose run'
+alias dcstahp 'docker-compose stop'
+alias dcd 'docker-compose down'
