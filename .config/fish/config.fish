b0e313ff45e3d1f8d78eaf4bb32f8eb3e071154f
JoshAshby <joshuaashby@joshashby.com>
2021-06-25 14:55:31 -0600

    updates

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,14 +1,12 @@
 set fish_greeting
 
+set PATH $HOME/bin $PATH
+set PATH /usr/local/bin $PATH
+
+set -x DOTS $HOME/repos/dots
+
 . ~/.config/fish/aliases.fish
 
 if test -e ~/.config/fish/custom.fish
   . ~/.config/fish/custom.fish
 end
-
-set PATH $HOME/bin $PATH
-
-set -x DOTS $HOME/repos/dots
-set -g fish_user_paths "/usr/local/opt/openjdk/bin" $fish_user_paths
-
-direnv hook fish | source
--- .config/fish/custom/mac.fish
+++ .config/fish/custom/mac.fish
@@ -2,7 +2,11 @@ set -g EDITOR gvim
 
 set -x PGDATA /usr/local/var/postgres
 set -x ES_JAVA_OPTS '-XX:-MaxFDLimit -Xms8g -Xmx8g'
+set -g fish_user_paths "/usr/local/opt/openjdk/bin" $fish_user_paths
 
 source ~/.iterm2_shell_integration.(basename $SHELL)
 
 source /usr/local/opt/asdf/asdf.fish
+
+direnv hook fish | source
+zoxide init fish | source
